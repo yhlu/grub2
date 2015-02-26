df46c3755b4b3a7302902a789d3f9f174b38ab4a
Yinghai Lu <yinghai@kernel.org>
2015-02-26 01:39:15 -0800

    x86, legacy: add grub_initrd_load_copy
    will use it load big initrd (that does not fit under 4g) into buffer above 4G.
    
    Signed-off-by: Yinghai Lu <yinghai@kernel.org>

--- include/grub/linux.h
+++ include/grub/linux.h
@@ -22,3 +22,8 @@ grub_initrd_close (struct grub_linux_initrd_context *initrd_ctx);
 grub_err_t
 grub_initrd_load (struct grub_linux_initrd_context *initrd_ctx,
 		  char *argv[], void *target);
+
+grub_err_t
+grub_initrd_load_copy (struct grub_linux_initrd_context *initrd_ctx,
+                  char *argv[], grub_uint64_t target, void * (*map_func)(unsigned long), void *buf);
+
